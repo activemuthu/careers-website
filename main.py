5fe45a886b8eb41545138155095d11f300c0b055
Muthuraja J <50619703+activemuthu@users.noreply.github.com>
2023-02-19 13:34:49 +0000

    Added Api code and open positions part

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Texas,USA',
+  'Salary': '3 LPA'
+}, {
+  'id': 2,
+  'title': 'Data science',
+  'location': 'Chennai,India',
+  'Salary': '5 LPA'
+}, {
+  'id': 3,
+  'title': 'Blockchain Developer',
+  'location': 'Chennai,India',
+  'Salary': '10 LPA'
+}, {
+  'id': 1,
+  'title': 'Python Developer',
+  'location': 'Chennai,India',
+  'Salary': '12 LPA'
+}]
 
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+  return render_template("index.html", jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
