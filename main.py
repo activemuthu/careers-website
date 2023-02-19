2d14d19f21979b2d6a4c94b6f6de19395c5d6a75
Muthuraja J <50619703+activemuthu@users.noreply.github.com>
2023-02-19 12:48:10 +0000

    Implemented bootstrap and css

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-  return 'Muthu'
+  return render_template("index.html")
 
-if __name__=='__main__':
-  app.run(host='0.0.0.0',debug=True)
 
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
